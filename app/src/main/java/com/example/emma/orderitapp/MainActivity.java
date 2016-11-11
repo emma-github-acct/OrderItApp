e6e18ad2a7a69c6a79d7be0fe64a7eaaa15b314b
Emma <emmalysek@gmail.com>
2016-11-10 23:27:10 -0500

    Adds text files and launch button for testing

--- app/src/main/java/com/example/emma/orderitapp/MainActivity.java
+++ app/src/main/java/com/example/emma/orderitapp/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.emma.orderitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+
+    public void scanBuisnessQR() {
+        /*
+        this.answerList = new ArrayList<String>();
+
+        try {
+
+            InputStream iFile = currentContext.getResources().openRawResource( R.raw.possible_answers );
+            Scanner scan = new Scanner( iFile );
+            while ( scan.hasNextLine() ) {
+                String stringInput = scan.nextLine();
+                answerList.add( stringInput );
+
+            }
+
+        } catch ( IllegalStateException ise ){
+            Toast.makeText( currentContext, R.string.input_error, Toast.LENGTH_LONG).show();
+
+        } catch ( IllegalArgumentException ire ){
+            Toast.makeText( currentContext, R.string.input_error, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    */
     }
 }
