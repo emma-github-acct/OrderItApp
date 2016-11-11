4327feca6fae5c7a0866301a302b5e877b4f413d
Emma <emmalysek@gmail.com>
2016-11-10 23:07:16 -0500

    Adds Buiness Hierarchy

---
+++ app/src/main/java/com/example/emma/orderitapp/Business.java
@@ -0,0 +1,25 @@
+package com.example.emma.orderitapp;
+
+/**
+ * Created by Emma on 11/10/16.
+ */
+
+public class Business {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public void  setName(String name){this.name = name;}
+    public String getName() {return name;}
+
+    public void  setEmail(String email){this.email = email;}
+    public String getEmail() {return email;}
+
+    public void  setPhone(String phone){this.phone = phone;}
+    public String getPhone() {return phone;}
+
+    public void  setAddress(String address){this.address = address;}
+    public String getAddress() {return address;}
+}
--- app/src/main/java/com/example/emma/orderitapp/Restaurant.java
+++ app/src/main/java/com/example/emma/orderitapp/Restaurant.java
@@ -4,25 +4,8 @@ package com.example.emma.orderitapp;
  * Restaurant model object.
  */
 
-public class Restaurant {
+public class Restaurant extends Business{
 
-    private String name;
-    private String email;
-    private String phone;
-    private String address;
-
-
-    public void  setName(String name){this.name = name;}
-    public String getName() {return name;}
-
-    public void  setEmail(String email){this.email = email;}
-    public String getEmail() {return email;}
-
-    public void  setPhone(String phone){this.phone = phone;}
-    public String getPhone() {return phone;}
-
-    public void  setAddress(String address){this.address = address;}
-    public String getAddress() {return address;}
 
 
 }
